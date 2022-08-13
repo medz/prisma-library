85e087f5ab222f66cafa6c5b24573c299a8726f6
Seven Du <whine.whitish.0u@icloud.com>
2022-08-13 15:03:48 +0800

    Update install-openssl.sh

--- .cross-buildkit/install-openssl.sh
+++ .cross-buildkit/install-openssl.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get install pkg-config libssl-dev openssl -y
+# Install LibreSSL
+apt-get update && apt-get install libssl-dev -y
+
+# Link configuration.h and opensslconf.h
+ln -s /usr/include/x86_64-linux-gnu/openssl/opensslconf.h /usr/include/openssl/opensslconf.h
+ln -s /usr/include/x86_64-linux-gnu/openssl/opensslconf.h /usr/include/openssl/opensslconf.h
